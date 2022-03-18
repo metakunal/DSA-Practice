51fd20786318f584e2c5e594a9218d4f512ebc7f
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-03-18 19:34:49 +0530

    Time: 5 ms (37.14%), Space: 6.6 MB (81.09%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -4,20 +4,26 @@ public:
         vector<int> last(26);
         vector<bool> visited(26);
         string ans="";
+        //Storing the last index of every character in a vector
         for(int i=0;i<s.length();i++)
         {
             last[s[i]-'a']=i;
         }
         for(int i=0;i<s.length();i++)
         {
+            //If already visited then skip
             if(visited[s[i]-'a'])
                 continue;
             
+            //Here we check if the character at end of answer is greater than current character
+            //and also we have occurences of that character after i
+            //then we pop it from the answer and mark its visited as false
             while(!ans.empty() && ans.back()>s[i] && last[ans.back()-'a']>i)
             {
                 visited[ans.back()-'a']=false;
                 ans.pop_back();
             }
+            //Push S[i] to ans and mark its visited as true.
             ans.push_back(s[i]);
             visited[s[i]-'a']=true;
         }
