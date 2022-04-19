1aa4a81ee5bd93b2a1c0f73cef2a9ad9b2245019
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-04-19 12:11:48 +0530

    Time: 3 ms (33.10%), Space: 6.6 MB (68.31%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
+       vector<vector<int>>res(n,vector<int>(n));
         int r1 = 0, r2 = n-1;
         int c1 = 0, c2 = n-1;
         int val = 0;
-		
-		// result matrix
-        vector<vector<int>> v(n, vector<int> (n));
-        while(r1 <= r2 && c1 <= c2)
+        while(r1<=r2 && c1<=c2)
         {
-            // left to right (row will be fixed)
-            for(int i = c1; i <= c2; ++i)
-                v[r1][i] = ++val;
-				
-				// move down(col will be fixed)
-            for(int i = r1+1; i <= r2; ++i)
-                v[i][c2] = ++val;
-				
-            // move right to left
-            // move  up
-            if(r1 < r2 && c1 < c2)
-            {
-                // move right to left (row will be fixed)
-                for(int i = c2-1; i>c1; --i)
-                    v[r2][i] = ++val;
-					
-					// move up (col will be fixed)
-					for(int i = r2; i>r1; --i) 
-                    v[i][c1] = ++val;
-            }
-            ++r1;
-            --r2;
-            ++c1;
-            --c2;
+            for(int i=c1;i<=c2;i++)//left to right move,row is fixed
+            res[r1][i] = ++val;
+       
+        for(int i=r1+1;i<=r2;i++) //top to bootom move ,colomn is fixed
+            res[i][c2] = ++val;
+       
+        for(int i=c2-1;i>=c1;i--) // right to left move,row is fixed
+            res[r2][i] = ++val;
+        
+        for(int i=r2-1;i>r1;i--) // bottom to up move ,colomn is fixed
+            res[i][c1] = ++val;
+            
+            r1++;
+            r2--;
+            c1++;
+            c2--;
         }
-        return v;
+        return res;
     }
-	// for github repository link go to my profile.
 };
