ff3d57540aaf7f760304db735cb093e4f5fcc8ec
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-04-21 17:15:54 +0530

    Time: 196 ms (32.64%), Space: 41.1 MB (74.20%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,37 +1,42 @@
 class MyHashSet {
-private:
-	int prime;
-	vector<list<int>> table;
-
-	int hash(int key) {
-		return key % prime;
-	}
-
-	list<int>::iterator search(int key) {
-		int h = hash(key);
-		return find(table[h].begin(), table[h].end(), key);
-	}
-
+    
 public:
-	MyHashSet() : prime(10007), table(prime) {}
-	
-	void add(int key) {
-		int h = hash(key);
-		if (!contains(key))
-			table[h].push_back(key);
-	}
-	
-	void remove(int key) {
-		int h = hash(key);
-		auto it = search(key);
-		if (it != table[h].end())
-			table[h].erase(it);
-	}
-	
-	bool contains(int key) {
-		int h = hash(key);
-		return search(key) != table[h].end();
-	}
+    vector<list<int>> m;
+    int size;
+    MyHashSet() {
+        size = 100;
+        m.resize(size);
+    }
+    int hash(int key)
+    {
+        return (key%size);
+    }
+    list<int>::iterator search(int key)
+    {
+        int i=hash(key);
+        return find(m[i].begin(),m[i].end(),key);
+    }
+    void add(int key) {
+        int i=hash(key);
+        if(contains(key))
+            return;
+        m[i].push_back(key);
+    }
+    
+    void remove(int key) {
+         int i=hash(key);
+        if(!contains(key))
+            return;
+        m[i].erase(search(key));
+    }
+    
+    bool contains(int key) {
+        int i=hash(key);
+        if(search(key)==m[i].end())
+            return false;
+        else
+            return true;
+    }
 };
 
 /**
