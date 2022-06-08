878d8a0a695ef048fbc446dea56c345db397564f
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-06-08 12:28:03 +0530

    Time: 8 ms (49.44%), Space: 6.1 MB (56.75%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
+    int ans=0;
+    bool isSafe(vector<string> &board,int row, int col)
+    {
+        int n=board.size();
+        for(int i=0;i<col;i++)
+        {
+            if(board[row][i]=='Q')
+                return false;
+        }
+        for(int i=row,j=col;i>=0&&j>=0;i--,j--)
+        {
+            if(board[i][j]=='Q')
+                return false;
+        }
+        for(int i=row,j=col;j>=0&&i<n;i++,j--)
+        {
+            if(board[i][j]=='Q')
+                return false;
+        }
+        return true;
+    }
+    void solve(vector<string> &board, int col){
+        // exit condition
+        if(col == board.size()){
+            ans++;
+            return;
+        }
+        // iterate every possible position
+        for(int i=0;i<board.size();i++){
+            if(isSafe(board,i,col)){
+                // make decision
+                board[i][col] = 'Q';
+                // next iteration
+                solve(board,col+1);
+                // back-tracking
+                board[i][col] = '.';
+            }
+        }
+    }
     int totalNQueens(int n) {
-	vector<vector<bool>> board(n, vector<bool>(n, false));
-	return solve(board, 0);
-}
-bool check(vector<vector<bool>>& board, int row, int col) {
-	int n = size(board);
-	for(int i = 0; i <= row; i++) {
-		if(board[i][col]) return false; // checking if any queen already placed on same column previously
-		// checking if all diagonals are safe -
-		if(row - i >= 0 && col - i >= 0 && board[row - i][col - i]) return false;
-		if(row - i >= 0 && col + i <  n && board[row - i][col + i]) return false;
-	}
-	return true;
-}    
-int solve(vector<vector<bool>>& board, int row) {
-	if(row == size(board)) return 1;
-	int count = 0;
-	for(int col = 0; col < size(board); col++)           
-		if(check(board, row, col)){          // check if we can place at (row, col)
-			board[row][col] = true;          // place the queen at (row, col)
-			count += solve(board, row + 1);  // explore for the next row. The function will return 1 if all N queens get placed for current combination
-			board[row][col] = false;         // backtrack - remove previously placed queen and try for different columns
-		}                                
-	return count;
-}
+         if(n <= 0) return {{}};
+        vector<string> board(n,string(n,'.'));
+        solve(board,0);
+        return ans;
+    }
 };
