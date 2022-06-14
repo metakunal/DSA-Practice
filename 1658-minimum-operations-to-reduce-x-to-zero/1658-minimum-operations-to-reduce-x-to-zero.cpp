f482070872ff8d05d99bda744b812d708d72affc
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-06-14 17:16:07 +0530

    Time: 888 ms (9.98%), Space: 168.2 MB (14.18%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,23 +1,21 @@
 class Solution {
-   public:
+public:
     int minOperations(vector<int>& nums, int x) {
-        int sum = 0, n = nums.size();
-        for (int i : nums) sum += i;
-        int target = sum - x;
-        int curr_sum = 0, max_len = 0;
-        int start_idx = 0;
-        bool found = false;
-        for (int end_idx = 0; end_idx < n; end_idx++) {
-            curr_sum += nums[end_idx];
-            while (start_idx <= end_idx && curr_sum > target) {
-                curr_sum -= nums[start_idx];
-                start_idx += 1;
-            }
-            if (curr_sum == target) {
-                found = true;
-                max_len = max(max_len, end_idx - start_idx + 1);
-            }
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        int target = sum-x;
+        unordered_map<int,int> mp;
+        int presum=0;
+        int res=INT_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            presum+=nums[i];
+            if(presum==target)
+                res=i+1;
+            if(mp.find(presum)==mp.end())
+                mp.insert({presum,i});
+            if(mp.find(presum-target)!=mp.end())
+                res=max(res,i-mp[presum-target]);
         }
-        return found ? n - max_len : -1;
+        return res==INT_MIN?-1:nums.size()-res;
     }
 };
