40fdd8fa2538e440e0bd6b1d81f70e2a219cb0dc
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-06-18 15:38:16 +0530

    Time: 3 ms (62.66%), Space: 6.2 MB (66.01%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n=s.length();
+        unordered_map<char,int> mp;
+        int max=INT_MIN;
+        char cmax;
+        string res=s;
+        for(auto c:s)
+        {
+            mp[c]++;
+            if(mp[c]>max)
+            {
+                cmax=c;
+                max=mp[c];
+            }
+
+        }
+                    cout<<cmax<<mp[cmax];
+
+        if(n%2==0)
+        {
+            if(mp[cmax]>=(n/2)+1)
+                return "";
+        }
+        else
+        {
+            if(mp[cmax]>(n/2)+1)
+                return "";
+        }
+        
+        int i=0;
+        while(mp[cmax])
+        {
+            res[i]=cmax;
+            mp[cmax]--;
+            i=i+2;
+        }
+        // cout<<"i"<<i;
+         for (auto c: mp) {
+            while (c.second > 0) {
+                if (i >= n) i = 1; // First time it reaches L, reset it. Won't be inf loop cause not looping on idx
+                res[i] = c.first;
+                c.second=c.second-1;
+                i += 2;
+            }
+        }
+        for(auto m:mp)
+        {
+            cout<<m.first<<m.second;
+        }
+        return res;
+    }
+};
