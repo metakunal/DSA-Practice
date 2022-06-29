7ed3ed49d5d9ae8643768fdc085d49379f2feeae
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-06-29 18:46:07 +0530

    Time: 507 ms (5.56%), Space: 25.2 MB (5.49%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+//    bool sortcol(const vector<int>& v1, const vector<int>& v2)
+// {
+//     return v1[1] < v2[1];
+// }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        vector<vector<int>> res;
+        sort(people.begin(), people.end(),[](vector<int> p1, vector<int> p2){
+            return p1[0] > p2[0] || (p1[0] == p2[0] && p1[1] < p2[1]);
+        });
+        for(auto p:people)
+        {
+           res.insert(res.begin()+p[1],p);
+        }
+        return res;
+    }
+};
