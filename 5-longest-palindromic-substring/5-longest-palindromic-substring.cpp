dc3e3f6b692f6fef3bcd6e06f00152aebdd85929
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-07-01 23:12:25 +0530

    Time: 748 ms (17.94%), Space: 30.3 MB (29.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,33 +1,41 @@
 class Solution {
 	public:
    	string longestPalindrome(string s) {   
-	    int n = s.size();
-    	int dp[n][n];
-    	
-	    memset(dp,0,sizeof(dp));
-    	int end=1;
-    	int strt=0;
-	
-	    for(int i=0;i<n;i++){
-	        dp[i][i] = 1;
-    	}
-	    for(int i=0;i<n-1;i++){
-	        if(s[i]==s[i+1]){
-    	    	dp[i][i+1]=1;
-        		strt=i;end=2;
-       		}
-	    }
-    	for(int j=2;j<n;j++){
-	        for(int i=0;i< n-j;i++){  
-           		int lft=i;
-            	int rght = i+j;
-        	    
-    	        if(dp[lft+1][rght-1]==1 && s[lft]==s[rght]) 
-	            {
-                	dp[lft][rght]=1; strt=i; end=j+1; 
-            	}        
-        	}
-    	}
-    return s.substr(strt, end);
+	    int n=s.length();
+        int ans=INT_MIN;
+        int start=0,end=0;
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        for(int g=0;g<n;g++)
+        {
+            for(int i=0,j=g;j<dp.size();i++,j++)
+            {
+                if(g==0)
+                {
+                    dp[i][j]=true;
+                }else if(g==1)
+                {
+                    if(s[i]==s[j])
+                        dp[i][j]=true;
+                }else
+                {
+                    if(s[i]==s[j]&&dp[i+1][j-1]==true)
+                        dp[i][j]=true;
+                }
+                if(dp[i][j]==true)
+                {
+                   if(j-i+1>ans)
+                   {
+                       ans=j-i+1;
+                       start=i,end=j;
+                   }
+                }
+                    
+            }
+        }
+        string res="";
+        for(int i=start;i<=end;i++)
+            res+=s[i];
+        return res;
+
 	}
 };
