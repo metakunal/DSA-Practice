ae1ad5e72ef25f0ac2b01ef329874013f1f577dc
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-07-01 23:09:13 +0530

    Time: 36 ms (31.63%), Space: 7.7 MB (48.03%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int countSubstrings(string& s) {
-       return tabulation(s);
-    }
-     int tabulation(string& s) {
-        vector<vector<int>> tab(s.size(), vector<int>(s.size()));
-        int count = 0;
-        for(int i = s.size() - 1; i >= 0; --i) {
-            for(int j = i; j < s.size(); ++j) {
-                if (i == j) {
-                    tab[i][j] = 1;
-                } else if (i + 1 == j) {
-                    tab[i][j] = s[i] == s[j] ? 1 : 0;
-                } else {
-                    tab[i][j] = s[i] == s[j] ? tab[i+1][j-1] : 0;
+    int countSubstrings(string s) {
+        int n=s.length();
+        int count=0;
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        for(int g=0;g<n;g++)
+        {
+            for(int i=0,j=g;j<dp.size();i++,j++)
+            {
+                if(g==0)
+                {
+                    dp[i][j]=true;
+                }else if(g==1)
+                {
+                    if(s[i]==s[j])
+                        dp[i][j]=true;
+                }else
+                {
+                    if(s[i]==s[j]&&dp[i+1][j-1]==true)
+                        dp[i][j]=true;
                 }
-                count += tab[i][j];
+                if(dp[i][j]==true)
+                    count++;
             }
         }
         return count;
