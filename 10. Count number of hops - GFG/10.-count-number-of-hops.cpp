9331098f58dfee88014875a133a991c9b36158a6
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-07-04 15:57:43 +0530

    Added solution - LeetHub

---
+++ 10. Count number of hops - GFG/10.-count-number-of-hops.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+    long long MOD=1000000007;
+    public:
+    //Function to count the number of ways in which frog can reach the top.
+    // vector<int> dp;
+    long long helper(int n,vector<long long> &dp)
+    {
+        if(n==0 ||n==1)
+        return 1;
+        if(n==2)
+        return n;
+        if(dp[n]!=-1)
+        return dp[n]%MOD;
+        return dp[n]=(helper(n-1,dp)%MOD+helper(n-2,dp)%MOD+helper(n-3,dp)%MOD)%MOD;
+        
+    }
+    long long countWays(int n)
+    {
+        vector<long long> dp(n+1,-1);
+        // dp.resize(n);
+        // for(int i=0;i<n;i++)
+            // dp[i]=-1;
+        // dp[0]=0;
+        return helper(n,dp);
+        // your code here
+        
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+    //taking testcases
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    //taking number of steps in stair
+		int n;
+		cin>>n;
+		Solution ob;
+		//calling function countWays()
+		cout << ob.countWays(n) << endl;
+	}
+    
+    return 0;
+    
+}
+  // } Driver Code Ends
