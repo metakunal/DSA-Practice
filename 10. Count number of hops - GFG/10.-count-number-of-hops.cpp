0cfdb71d64851c9093b6412379c6a06ec0e4e837
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-07-04 16:02:41 +0530

    Added solution - LeetHub

--- 10. Count number of hops - GFG/10.-count-number-of-hops.cpp
+++ 10. Count number of hops - GFG/10.-count-number-of-hops.cpp
@@ -6,32 +6,26 @@ using namespace std;
 
 class Solution
 {
-    long long MOD=1000000007;
+    long long MOD=pow(10,9)+7;
     public:
     //Function to count the number of ways in which frog can reach the top.
-    // vector<int> dp;
-    long long helper(int n,vector<long long> &dp)
-    {
-         if(dp[n]!=-1)
-        return dp[n]%MOD;
-        if(n==0 ||n==1)
-        return 1;
-        if(n==2)
-        return n;
-       
-        return dp[n]=(helper(n-1,dp)%MOD+helper(n-2,dp)%MOD+helper(n-3,dp)%MOD)%MOD;
-        
-    }
     long long countWays(int n)
     {
-        vector<long long> dp(n+1,-1);
-        // dp.resize(n);
-        // for(int i=0;i<n;i++)
-            // dp[i]=-1;
-        // dp[0]=0;
-        return helper(n,dp);
+        long long first=1,second=1,third=2;
         // your code here
-        
+        if(n==1)
+        return second;
+        if(n==2)
+        return third;
+        long long ans=0;
+        for(int i=3;i<=n;i++)
+        {
+            ans=(first%MOD+second%MOD+third%MOD)%MOD;
+            first=second;
+            second=third;
+            third=ans;
+        }
+        return ans;
     }
 };
 
