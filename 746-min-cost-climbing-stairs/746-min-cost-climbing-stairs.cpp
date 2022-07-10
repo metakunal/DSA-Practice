185776aaab51c122be7bd3cb8ffc509900250920
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-07-10 12:25:06 +0530

    Time: 15 ms (27.88%), Space: 13.9 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -9,7 +9,16 @@ public:
         return dp[i]=cost[i]+min(helper(cost,i+1,dp),helper(cost,i+2,dp));   
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size()+1,-1);
-        return min(helper(cost,0,dp),helper(cost,1,dp));
+        // return min(helper(cost,0,dp),helper(cost,1,dp));
+                vector<int> dp(cost.size(),-1);
+int n=cost.size();
+        for(int i=0;i<n;i++)
+        {
+            if(i<2)
+                dp[i]=cost[i];
+            else
+                dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        }
+        return min(dp[n-1],dp[n-2]);
     }
 };
